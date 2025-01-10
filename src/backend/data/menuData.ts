c604683410045c71be6426dd963339539a0aec97
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-01-09 22:47:58 -0500

    Updated data for Nav menu

---
+++ src/backend/data/menuData.ts
@@ -0,0 +1,25 @@
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faHouse, faLightbulb, faBriefcase, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+
+export const menuData = [
+    {
+        title: 'Home',
+        icon: faHouse,
+        url: '/home'
+    },
+    {
+        title: 'Projects',
+        icon: faLightbulb,
+        url: '/projects'
+    },
+    {
+        title: 'About',
+        icon: faBriefcase,
+        url: '/about'
+    },
+    {
+        title: 'Contact',
+        icon: faAddressCard,
+        url: '/contact'
+    }
+];
