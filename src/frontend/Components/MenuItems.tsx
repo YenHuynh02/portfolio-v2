e21d2602afd16b29224c841c38c8bae01550ec34
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2025-01-10 23:28:04 -0500

    Add 2 more objects to set up active item in Nav

--- src/frontend/Components/MenuItems.tsx
+++ src/frontend/Components/MenuItems.tsx
@@ -1,1 +1,37 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface MenuItems {
+    title: string;
+    icon?: any;
+    url: string;
+}
+
+interface MenuItemsProps {
+    items: MenuItems;
+    activeItem: string;
+    setActiveItem: (title: string) => void;
+}
+
+const MenuItems: React.FC<MenuItemsProps> = ({ items, activeItem, setActiveItem }) => {
+    return (
+        <li>
+            <span>
+                {items.icon && <FontAwesomeIcon icon={items.icon} />}
+            </span>
+            <a
+                href={items.url}
+                className={activeItem === items.title ? 'active' : ''}
+                onClick={(e) => {
+                    setActiveItem(items.title)
+                    e.preventDefault();
+                }
+                }
+            >
+                {items.title}
+            </a>
+        </li >
+    );
+}
+
+export default MenuItems;
